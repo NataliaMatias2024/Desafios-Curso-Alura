820669bb527ad12b96b3786e68b385521fe6a735
Natalia Matias <dontreplay@github.com>
2024-01-26 21:57:45 -0300

    Conflito?

--- Testes.js
+++ Testes.js
@@ -1,7 +1,7 @@
 let listaCompras = [];
 
-listaCompras.push("Maçã");
+listaCompras.push("Maçã Verde");
 listaCompras.push("Arroz");
 listaCompras.push("Leite");
-listaCompras.push("Pão");
-listaCompras.push("Carne");
+listaCompras.push("Pão de fermentação natural");
+listaCompras.push("Carne de soja");
